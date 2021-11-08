e5518236380ae3eebfa5a176c590de245d7b2ba8
Chase Covington <mchasecov@gmail.com>
2021-11-08 18:57:19 -0500

    Added 31 Airport Seed records

---
+++ db/seeds/001_airports.rb
@@ -0,0 +1,41 @@
+# This file contains all record creation needed to seed the database with Airport records.
+# Each airport consists of:
+# ID
+# Code: A 3 letter string designating the airport name
+# Timestamps
+
+Airport.create(
+  [
+    { code: 'ATL' },
+    { code: 'DFW' },
+    { code: 'DEN' },
+    { code: 'ORD' },
+    { code: 'LAX' },
+    { code: 'CLT' },
+    { code: 'LAS' },
+    { code: 'PHX' },
+    { code: 'MCO' },
+    { code: 'SEA' },
+    { code: 'MIA' },
+    { code: 'IAH' },
+    { code: 'JFK' },
+    { code: 'FLL' },
+    { code: 'SFO' },
+    { code: 'EWR' },
+    { code: 'MSP' },
+    { code: 'DTW' },
+    { code: 'BOS' },
+    { code: 'PHL' },
+    { code: 'STL' },
+    { code: 'BWI' },
+    { code: 'TPA' },
+    { code: 'SAN' },
+    { code: 'SLC' },
+    { code: 'IAD' },
+    { code: 'BNA' },
+    { code: 'LGA' },
+    { code: 'DAL' },
+    { code: 'DCA' },
+    { code: 'PDX' }
+  ]
+)
