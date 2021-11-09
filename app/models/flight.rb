2634ef526c32014667ab0c3e823864033a67c6de
Chase Covington <mchasecov@gmail.com>
2021-11-09 11:06:42 -0500

    Added passenger_count validation to Flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,7 @@
 class Flight < ApplicationRecord
+  # Validation
+  validates :passenger_count, numericality: { less_than_or_equal_to: 100, only_integer: true }
+
   # Associations
 
   belongs_to :departure_airport, class_name: 'Airport'
