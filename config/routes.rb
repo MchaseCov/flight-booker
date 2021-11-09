4d4e16aa3f018c1192e28d447916bcac510fccb0
Chase Covington <mchasecov@gmail.com>
2021-11-08 22:35:06 -0500

    Added routes for flights, airports

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'flights/index'
+  root to: 'flights#index'
+
+  resources :flights, only: %i[index show]
+  resources :airports, only: %i[index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
