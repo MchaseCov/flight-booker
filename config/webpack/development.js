c14a419e74fe6fe251006f264626a84e66e58ff0
Chase Covington <mchasecov@gmail.com>
2021-11-09 20:10:33 -0500

    Added Jquery (unsure if this mattered)

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,3 +3,22 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+const webpack = require('webpack')
+
+module.exports = {
+    resolve: {
+        alias: {
+            $: 'jquery/src/jquery',
+            jQuery: 'jquery/src/jquery',
+            jquery: 'jquery',
+            'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ]
+}
