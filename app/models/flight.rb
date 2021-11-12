6bf078aa0747e1618ac2c8757bc06859c24e82eb
Chase Covington <mchasecov@gmail.com>
2021-11-11 19:39:34 -0500

    Added flight model associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -6,6 +6,8 @@ class Flight < ApplicationRecord
 
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+  has_many :bookings, dependent: :destroy
+  has_many :passengers, through: :bookings
 
   # Methods
 
