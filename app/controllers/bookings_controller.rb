eb2371a6a54475930c84a8d0b51c98cc0bc33f05
Chase Covington <mchasecov@gmail.com>
2021-11-12 00:45:31 -0500

    Added logic to create and save a Booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,36 @@
 class BookingsController < ApplicationController
-  before_action :flight_choice_params, only: %i[new]
+  before_action :fetch_params, only: %i[new create]
 
   def new
-    @flight_choice = Flight.where(id: params[:flight_id])
-    @passenger_count = params[:passenger_count]
+    @booking = Booking.new
+    @flight_choice = Flight.find(params[:flight_choice])
+    @passenger_count = params[:passenger_count].to_i
+    @passenger_count.times { @booking.passengers.build }
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      flash[:notice] = 'SUCCESS MESSAGE'
+      redirect_to @booking
+    else
+      flash.now[:alert] = @booking.errors.full_messages
+      render :new
+    end
+  end
 
+  def show
+    @booking = Booking.find(params[:id])
   end
 
   private
 
-  def flight_choice_params
-    params.permit(:flight_id, :passenger_count)
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: %I[id name email])
+  end
+
+  def fetch_params
+    @flight_choice = Flight.find(params[:flight_choice])
+    @passenger_count = params[:passenger_count].to_i
   end
 end
