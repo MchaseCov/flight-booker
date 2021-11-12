88daafb7afe4a1a9b6f1e6de1dc9dadb8737a4f6
Chase Covington <mchasecov@gmail.com>
2021-11-12 15:18:09 -0500

    Updated booking routes now shallow listed by flights

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'flights#index'
 
-  resources :flights, only: %i[index show]
-  resources :bookings, only: %i[new create show]
+  resources :flights, only: %i[index show] do
+    resources :bookings, only: %i[new create show], shallow: true
+  end
 
   get '/search/' => 'flights#search', :as => :flights_search
   get '/update_airports' => 'flights#update_airports', :as => :update_airports
