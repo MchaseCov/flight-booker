33a3836c17bf4652866577463bcccf209077752a
Chase Covington <mchasecov@gmail.com>
2021-11-11 19:40:06 -0500

    Added new + strong params to booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,18 @@
 class BookingsController < ApplicationController
+  before_action :flight_choice_params, only: %i[new]
+
+  def new
+    @flight_choice = Flight.where(id: params[:flight_id])
+    @passenger_count = params[:passenger_count]
+  end
+
+  def create
+
+  end
+
+  private
+
+  def flight_choice_params
+    params.permit(:flight_id, :passenger_count)
+  end
 end
