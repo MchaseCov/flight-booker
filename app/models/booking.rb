b734d79b91595197b5d87b724c03a6fd1c6af642
Chase Covington <mchasecov@gmail.com>
2021-11-11 19:39:45 -0500

    Added booking model associations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,9 @@
 class Booking < ApplicationRecord
+  # Validations
+
+  # Associations
+  belongs_to :flight
+  has_many :passengers, dependent: :destroy
+
+  accepts_nested_attributes_for :passenger
 end
