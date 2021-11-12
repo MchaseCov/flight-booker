4cb631b67df0f2cc7a75ba016176d3b70139eea2
Chase Covington <mchasecov@gmail.com>
2021-11-12 00:50:24 -0500

    Added callbacks, they quit working randomly

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,10 +5,25 @@ class Booking < ApplicationRecord
   # flight_id: :integer
   # timestamps: :integers
 
+  # Callbacks
+  after_commit :update_flight_passenger_total
+  after_destroy :restore_flight_passenger_total
   # Validations
 
   # Associations
   belongs_to :flight
   has_many :passengers, dependent: :destroy, inverse_of: :booking
   accepts_nested_attributes_for :passengers
+
+  # Methods
+
+  def update_flight_passenger_total
+    total_passengers = flight.passenger_count + passengers.count
+    flight.assign_attributes(passenger_count: total_passengers)
+  end
+
+  def restore_flight_passenger_total
+    total_passengers = flight.passenger_count - passengers.count
+    flight.assign_attributes(passenger_count: total_passengers)
+  end
 end
