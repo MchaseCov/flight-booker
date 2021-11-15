7d5d7ae77ace61f12ecb2a201cd69be6e1acf447
Chase Covington <mchasecov@gmail.com>
2021-11-15 17:54:21 -0500

    Moved flight attributes into reusable method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,17 +4,13 @@ class BookingsController < ApplicationController
   def new
     @booking = @flight_choice.bookings.new
     @passenger_count.times { @booking.passengers.build }
-    @departure_airport = @flight_choice.departure_airport.code
-    @arrival_airport = @flight_choice.arrival_airport.code
-    @departure_time = @flight_choice.departure_time.strftime('on %m-%d-%Y, at %H:%M')
-    @arrival_time = (@flight_choice.departure_time + @flight_choice.duration * 60 * 60)
-                    .strftime('on %m-%d-%Y, at %H:%M')
+    booked_flight_attributes(@flight_choice)
   end
 
   def create
     @booking = @flight_choice.bookings.new(booking_params)
     if @booking.save
-      redirect_to [@flight, @booking], notice: 'Signed you up!'
+      redirect_to [@flight, @booking], notice: 'Your flight is booked! Here is the info. A reciept has (not) been emailed to you!'
     else
       render :new, alert: @booking.errors.full_messages
     end
@@ -22,6 +18,8 @@ class BookingsController < ApplicationController
 
   def show
     @booking = Booking.find(params[:id])
+    @passenger_count = @booking.passengers.count
+    booked_flight_attributes(@booking.flight)
   end
 
   private
@@ -33,5 +31,13 @@ class BookingsController < ApplicationController
   def set_booking_attributes
     @flight_choice = Flight.find(params[:flight_id])
     @passenger_count = params[:tickets].present? ? params[:tickets].to_i : booking_params.to_h.length
+  end
+
+  def booked_flight_attributes(flight)
+    @departure_airport = flight.departure_airport.code
+    @arrival_airport = flight.arrival_airport.code
+    @departure_time = flight.departure_time.strftime('on %m-%d-%Y, at %H:%M')
+    @arrival_time = (flight.departure_time + flight.duration * 60 * 60)
+                    .strftime('on %m-%d-%Y, at %H:%M')
   end
 end
